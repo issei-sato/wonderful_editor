296a9daac70da6306a7fb1942804f1c829d38b2d
さとういっせい <>
2024-05-14 21:40:35 +0900

    dependent: destroyの後ろにasyncを追加。

--- app/models/article.rb
+++ app/models/article.rb
@@ -19,6 +19,6 @@
 #
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :article_likes, dependent: destroy
-  has_many :comments, dependent: destroy
+  has_many :article_likes, dependent: destroy_async
+  has_many :comments, dependent: destroy_async
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  has_many :articles, dependent: destroy
-  has_many :article_likes, dependent: destroy
-  has_many :comments, dependent: destroy
+  has_many :articles, dependent: destroy_async
+  has_many :article_likes, dependent: destroy_async
+  has_many :comments, dependent: destroy_async
 end
